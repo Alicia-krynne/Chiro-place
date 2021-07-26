903b0be5e7ae1328922504041dbdbf4669ed3481
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-07-26 22:57:31 +0300

    models for profile ,comments and Produce created

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Produce, Comment,Profile
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Produce)
+admin.site.register(Comment)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name',  'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Profile(models.Model):
+  FARMER = 1
+  CONSUMER =2
+    
+  PROFILE_TYPE=(
+  (FARMER, 'Farmer'),
+  (CONSUMER,'consumer'),
+  )   
+  profile_picture = CloudinaryField("profilepics",null= True)
+  name = models.CharField(max_length=255)
+  bio = models.CharField(max_length=1000)
+  profile_type = models.PositiveSmallIntegerField(choices=PROFILE_TYPE, blank=True, null=True)
+  Phone_number = models.EmailField()
+
+class Produce(models.Model):
+  PRODUCE_UNIT = (
+        ('LT', 'Litres'),
+        ('KG', 'Kilograms'),
+        ('C', 'Crates'),
+        ('E', 'Each'),
+    )
+  Produce_type =models.CharField(max_length=255)
+  produce_description = models.CharField(max_length=255)
+  produce_unit = models.CharField(max_length=2, choices=PRODUCE_UNIT)
+  produce_price =models.CharField(max_length=255)
+  produce_location =models.CharField(max_length=255)
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Produce,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
+
+
+  
