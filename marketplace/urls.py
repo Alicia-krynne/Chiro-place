d0881a0395143aa820a7210fe47660517574dc1a
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-07-29 05:40:18 +0300

    add django -registration

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-
+from . import views
 from .views import ProduceView,ProfileView
 
 
@@ -8,8 +8,10 @@ app_name = "marketplace"
 
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
-    path('produce/', ProduceView.as_view()),
-    path('profiles/',ProfileView.as_view()),
+  path('',views.welcome,name='Welcome'),
+  # path('search/', views.search_results, name='search_results'),
+  path('produce/', ProduceView.as_view()),
+  path('profiles/',ProfileView.as_view()),
 #     path('profile/<int:pk>',ProduceView.as_view()),
 #     path('produce/<int:pk>',ProduceView.as_view()),
   ]
